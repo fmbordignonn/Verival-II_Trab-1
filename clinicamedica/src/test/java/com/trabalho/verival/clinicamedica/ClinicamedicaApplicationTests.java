cfedf7470146824b4dd8cc59a8e9a9c0787bf6a1
unknown <felipe.bordignon@edu.pucrs.br>
2020-10-27 19:35:49 -0300

    projeto base

---
+++ clinicamedica/src/test/java/com/trabalho/verival/clinicamedica/ClinicamedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.verival.clinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicamedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
